8b81ed49bd8401e6d22a01b0827a4ad975b76621
alokkkxpixel <alokpithale4@gmail.com>
2025-11-10 23:17:22 +0530

    db connect

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,15 @@
 const mongoose = require( "mongoose");
 
 module.exports.connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+  
+    try {
+     await mongoose.connect(process.env.MONGO_ATLAS_URI, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     });
+    console.log(" MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.error("MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
-};
+}
